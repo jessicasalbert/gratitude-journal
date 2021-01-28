6c9f19a1fd43e3a6932d9886ba736a1da187c754
jessicasalbert <jessicarsalbert@gmail.com>
2021-01-28 09:24:11 -0500

    Add login page background image

--- pages/login.js
+++ pages/login.js
@@ -1,8 +1,11 @@
 import styled from 'styled-components';
 
+
 const Login = () => {
     return (
-        <LoginStyle>Login</LoginStyle>
+        <LoginStyle>
+            Login
+        </LoginStyle>
     )
 }
 
@@ -10,6 +13,21 @@ const Login = () => {
 export default Login;
 
 const LoginStyle = styled.div`
-  background-image: 
+    
+
+    ::after {    
+        content: "";
+        background-image: url("/thankful.jpg");
+        background-size: cover;
+        height: 100vh;
+        -webkit-filter: grayscale(100%); /* Safari 6.0 - 9.0 */
+        filter: grayscale(100%);
+        position: absolute;
+        top: 0px;
+        right: 0px;
+        bottom: 0px;
+        left: 0px;
+        opacity: 0.50;
+        background-position: center, center;
 `;
 
