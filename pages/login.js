d0224f6ff3bfcec52027196fc76341d3370e4fda
jessicasalbert <jessicarsalbert@gmail.com>
2021-01-28 08:24:27 -0500

    Configure styled components

---
+++ pages/login.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const Login = () => {
+    return (
+        <LoginStyle>Login</LoginStyle>
+    )
+}
+
+
+export default Login;
+
+const LoginStyle = styled.div`
+  background-image: 
+`;
+
