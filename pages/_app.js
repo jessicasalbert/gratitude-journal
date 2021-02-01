ad39aeca04e297ee60983b6f20ab9da15b4c547e
jessicasalbert <jessicarsalbert@gmail.com>
2021-01-31 21:38:43 -0500

    Add login form

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,12 @@
 import '../styles/globals.css'
-import Navbar from '../components/nav'
+import Navbar from '../components/nav';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-    <Component {...pageProps} />
     <Navbar/>
+    <Component {...pageProps} />
     </>
   )
 }
--- pages/login.js
+++ pages/login.js
@@ -1,11 +1,37 @@
 import styled from 'styled-components';
-
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container'
 
 const Login = () => {
+
+    
+
+
     return (
         <LoginStyle>
-            
-            
+            <Container className="w-50 p-3" style={loginBoxStyle}>
+            <Form>
+                <Form.Group controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Text className="text-muted">
+                    We'll never share your email with anyone else.
+                    </Form.Text>
+                </Form.Group>
+
+                <Form.Group controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" />
+                </Form.Group>
+                <Form.Group controlId="formBasicCheckbox">
+                    <Form.Check type="checkbox" label="Check me out" />
+                </Form.Group>
+                <Button variant="dark" type="submit">
+                    Submit
+                </Button>
+                </Form>
+                </Container>
         </LoginStyle>
     )
 }
@@ -15,6 +41,7 @@ export default Login;
 
 const LoginStyle = styled.div`
     ::after {    
+        z-index: -1;
         content: "";
         background-image: url("/thankful.jpg");
         background-size: cover;
@@ -38,6 +65,8 @@ const LoginStyle = styled.div`
     
 ;
 
-const LoginBoxStyle = styled.div`
-
-`
+const loginBoxStyle = {
+    backgroundColor: "white",
+    opacity: 0.8,
+    marginTop: "15vh"
+}
