de9baac9e63e31dd527eb9618d9c196932a08178
jessicasalbert <jessicarsalbert@gmail.com>
2021-02-07 19:52:36 -0500

    add mock db

---
+++ db.js
@@ -0,0 +1,18 @@
+import loadCustomRoutes from "next/dist/lib/load-custom-routes"
+
+const db = [
+    {
+        user: "jessica",
+        post:  "Our books, guides, and videos have helped thousands of your peers get better at their jobs. No fluff or sales pitches. Just quality information and insights.Our books, guides, and videos have helped thousands of your peers get better at their jobs. No fluff or sales pitches. Just quality information and insights.Our books, guides, and videos have helped thousands of your peers get better at their jobs. No fluff or sales pitches. Just quality information and insights."
+    },
+    {
+        user: "phil",
+        post:  "I think my favorite thing about Django is the name. There is so much pun potential.I think my favorite thing about Django is the name. There is so much pun potential.I think my favorite thing about Django is the name. There is so much pun potential.I think my favorite thing about Django is the name. There is so much pun potential."
+    },
+    {
+        user: "rous",
+        post:  "Our books, guides, and videos have helped thousands of your peers get better at their jobs. No fluff or sales pitches. Just quality information and insights.Our books, guides, and videos have helped thousands of your peers get better at their jobs. No fluff or sales pitches. Just quality information and insights.Our books, guides, and videos have helped thousands of your peers get better at their jobs. No fluff or sales pitches. Just quality information and insights."
+    }
+]
+
+export default db
